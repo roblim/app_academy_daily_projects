fce57044a63254407885648c1eaa48e694aec747
Rob Lim <robert.m.lim@gmail.com>
2017-08-04 11:39:14 -0700

    added set parent method

--- w1d5/poly_tree_node/lib/00_tree_node.rb
+++ w1d5/poly_tree_node/lib/00_tree_node.rb
@@ -1,3 +1,19 @@
 class PolyTreeNode
+  attr_reader :value
+  attr_accessor :parent, :children
+
+  def initialize(value, children = [], parent = nil)
+    @value = value
+    @children = children
+    @parent = parent
+  end
+
+  def parent=(new_parent)
+    return if new_parent == @parent
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = new_parent
+    new_parent.children << self unless @parent.nil?
+  end
+
 
 end
