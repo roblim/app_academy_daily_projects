8d079493d60dfe91fe07668142787d1a364999b9
Rob Lim <robert.m.lim@gmail.com>
2017-08-03 18:17:12 -0700

    added bomb_idx default value

--- w1d4/minesweeper/tile.rb
+++ w1d4/minesweeper/tile.rb
@@ -2,7 +2,7 @@ class Tile
 
   attr_accessor :visible, :bomb, :bomb_idx
 
-  def initialize(bomb, visible, bomb_idx)
+  def initialize(bomb, visible, bomb_idx = 0 )
     @bomb = bomb
     @visible = visible
     @bomb_idx = bomb_idx
