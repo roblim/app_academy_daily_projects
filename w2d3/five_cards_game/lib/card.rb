7081978a8f824bfd8202aad428821696a16e320c
Rob Lim <robert.m.lim@gmail.com>
2017-08-09 16:45:57 -0700

    done with deck object

--- w2d3/five_cards_game/lib/card.rb
+++ w2d3/five_cards_game/lib/card.rb
@@ -1,12 +1,15 @@
 class Card
   attr_reader :value, :suit
 
-  SUITS = [:diamond, :spade, :club, :heart]
 
   def initialize(value, suit)
     raise ArgumentError, 'Incorrect value' unless (1..13).include?(value)
-    raise ArgumentError, 'Not a suit' unless SUITS.include?(suit)
+    raise ArgumentError, 'Not a suit' unless Card.valid_suits.include?(suit)
     @value = value
     @suit = suit
+  end
+
+  def self.valid_suits
+    %i(diamond spade club heart)
   end
 end
