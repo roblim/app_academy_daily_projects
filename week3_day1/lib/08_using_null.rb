35a544c4b431fd36fa1fbd8a127fdc656fa573ca
Rob_Lim <robert.m.lim@gmail.com>
2017-08-18 14:19:03 -0700

    Complete active_record_lite phase 1

--- week3_day1/lib/08_using_null.rb
+++ week3_day1/lib/08_using_null.rb
@@ -18,6 +18,14 @@ require_relative './sqlzoo.rb'
 def null_dept
   # List the teachers who have NULL for their department.
   execute(<<-SQL)
+
+    SELECT
+      name
+    FROM
+      teachers
+    WHERE
+      dept_id IS NULL;
+
   SQL
 end
 
@@ -25,6 +33,14 @@ def all_teachers_join
   # Use a type of JOIN that will list all teachers and their department,
   # even if the department in NULL/nil.
   execute(<<-SQL)
+
+    SELECT
+      teachers.name, depts.name
+    FROM
+      teachers
+      LEFT JOIN
+        depts ON teachers.dept_id = depts.id;
+
   SQL
 end
 
