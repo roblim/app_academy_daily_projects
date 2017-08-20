ba5e270c9570045d30d51eb58af40001ab85a619
Rob_Lim <robert.m.lim@gmail.com>
2017-08-20 00:12:26 -0700

    Begin work on w3d5 associatable and complete w3d1 sqlzoo

--- week3_day5/activerecord_lite_part1/lib/02_searchable.rb
+++ week3_day5/activerecord_lite_part1/lib/02_searchable.rb
@@ -3,10 +3,21 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map { |attribute| "#{attribute} = ?" }.join(" AND ")
+    values = params.values
+    result = DBConnection.execute(<<-SQL, *values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+    result.map { |res| self.new(res) }
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
--- week3_day5/activerecord_lite_part1/lib/03_associatable.rb
+++ week3_day5/activerecord_lite_part1/lib/03_associatable.rb
@@ -10,31 +10,55 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.to_s.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    f_key = options[:foreign_key]
+    p_key = options[:primary_key]
+    c_name = options[:class_name]
+
+    f_key ? self.foreign_key = f_key : self.foreign_key = "#{name}_id".to_sym
+    p_key ? self.primary_key = p_key : self.primary_key = :id
+    c_name ? self.class_name = c_name : self.class_name = name.capitalize
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    f_key = options[:foreign_key]
+    p_key = options[:primary_key]
+    c_name = options[:class_name]
+
+    f_key ? self.foreign_key = f_key : self.foreign_key = "#{self_class_name.downcase}_id".to_sym
+    p_key ? self.primary_key = p_key : self.primary_key = :id
+    c_name ? self.class_name = c_name : self.class_name = name.singularize.capitalize
+
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+      
   end
+
+  # options = BelongsToOptions.new(name, options)
+  # foreign_key = options.send(:foreign_key)
+  # define_method(name) do
+  #   result = DBConnection.execute(<<-SQL, foreign_key)
+  #     SELECT *
+  #     FROM #{options.table_name}
+  #     WHERE id = ?
+  #   SQL
+  #   options.model_class.new(result.first)
+  # end
 
   def has_many(name, options = {})
     # ...
@@ -46,5 +70,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
