0c6ab148ddb5106e8b943893768e994e912563c9
Rob_Lim <robert.m.lim@gmail.com>
2017-08-22 11:46:22 -0700

    Initial commit

---
+++ app/models/cat.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Cat < ApplicationRecord
+end
