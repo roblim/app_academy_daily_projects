b681d4c5e440ac2b71586cc2e7ecf7bce4c63221
Rob_Lim <robert.m.lim@gmail.com>
2017-08-22 16:13:24 -0700

    Complete phase 1

---
+++ app/models/cat_rental_request.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: cat_rental_requests
+#
+#  id         :integer          not null, primary key
+#  cat_id     :integer          not null
+#  start_date :date             not null
+#  end_date   :date             not null
+#  status     :string           default("PENDING"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class CatRentalRequest < ApplicationRecord
+  validates :cat_id, :start_date, :end_date, :status, presence: true
+  validates :status, inclusion: { in: %w(PENDING APPROVED DENIED),
+                                  message: "is invalid" }
+
+  belongs_to :cat,
+    foreign_key: :cat_id,
+    primary_key: :id,
+    class_name: :Cat
+end
