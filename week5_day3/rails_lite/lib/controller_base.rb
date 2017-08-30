7f35f4c94e51f1cec737fc8c14e2fe2fb1a3a5e2
Rob Lim <robert.m.lim@gmail.com>
2017-08-30 14:42:25 -0700

    Complete rails lite phase 3

--- week5_day3/rails_lite/lib/controller_base.rb
+++ week5_day3/rails_lite/lib/controller_base.rb
@@ -1,6 +1,7 @@
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
+require 'active_support/inflector'
 require_relative './session'
 
 class ControllerBase
@@ -8,25 +9,39 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "Cannot render content twice!" if @already_built_response
+    @res["Location"] = url
+    @res.status = 302
+    @already_built_response = !@already_built_response
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Cannot render content twice!" if @already_built_response
+    res['Content-Type'] = content_type
+    res.write(content)
+    @already_built_response = !@already_built_response
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+
+
   end
 
   # method exposing a `Session` object
@@ -37,4 +52,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
