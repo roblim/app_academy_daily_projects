8eac9cf299d4049106fe9b365c0dc4ed8c6c37a8
Rob Lim <robert.m.lim@gmail.com>
2017-09-05 16:53:57 -0700

    Complete tic tac toe

--- week6_day2/js/main.js
+++ week6_day2/js/main.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js');
+const Game = require('./ttt_node/game.js');
 
 $( () => {
-  // Your code here
+  const $gridCont = $(".ttt");
+  const game = new Game();
+  const view = new View(game, $gridCont);
 });
